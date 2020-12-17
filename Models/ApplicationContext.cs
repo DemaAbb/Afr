0579d41e1694164592def120c50038d823158ed9
DemaAbb <72205763+DemaAbb@users.noreply.github.com>
2020-12-17 12:55:07 +0300

    Add User and Role Models, and file ApplicationConntext

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication11.Models
+{
+    public class ApplicationContext: DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        
+            : base(options)
+        
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,21 +1,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication11.Models;
 
 namespace WebApplication11
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=usersdb;Trusted_Connection=True;";
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
